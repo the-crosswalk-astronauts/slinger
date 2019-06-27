e6fd6213139a4dfcfa2dc956ee7be8281992c363
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-06-27 16:31:26 -0600

    Sidebar styling fixes

--- src/Components/UserView/Sidebar.jsx
+++ src/Components/UserView/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {contracts} from './AndrewDummy/sidebarDummy'
+import React, { Component } from 'react'
+import { deals } from '../../dummyData'
 
 
 export default class Sidebar extends Component {
@@ -7,17 +7,22 @@ export default class Sidebar extends Component {
     return (
       <div className='Sidebar'>
         <div className="search-hold">
-          <input type="text" placeholder='Search your contracts'/>
+          <input type="text" placeholder='Search your contracts' />
         </div>
         <div className="sidebar-cards-hold">
-          {contracts.map(element => (
-            <div className='sidebar-card'>
+          {deals.map(element => (
+            <div key={element.id} className='sidebar-card'>
               <p>{element.buyer}</p>
               <p>{element.address}</p>
             </div>
           ))}
         </div>
-        <div className="sidebar-nav-hold"></div>
+        <nav className="sidebar-nav-hold">
+          <div className="sidebar-nav-item">home</div>
+          <div className="sidebar-nav-item">add</div>
+          <div className="sidebar-nav-item">vault</div>
+          <div className="sidebar-nav-item">logout</div>
+        </nav>
       </div>
     )
   }
--- src/dummyData.js
+++ src/dummyData.js
@@ -1,122 +1,123 @@
-let id=0
-let toDoId=0
+let id = 0
+let toDoId = 0
 
-module.exports= [
+module.exports = {
+  deals: [
   {
     id: ++id,
-    address: '512 N Claypool Ct VA Beach VA', 
+    address: '512 N Claypool Ct VA Beach VA',
     mls: 13288899,
-    price: 250000, 
-    lender:'Travis Carter', 
-    lenderPhone: 801339999, 
-    lenderEmail: 'travis@firstcolonymortgage.com', 
+    price: 250000,
+    lender: 'Travis Carter',
+    lenderPhone: 801339999,
+    lenderEmail: 'travis@firstcolonymortgage.com',
     title: "Jami Decker",
     titlePhone: 8012234400,
-    titleEmail: 'jdecker@inwesttitle.com', 
-    listingAgent: 'Mark Bishop', 
-    listingAgentPhone: 8016024201, 
+    titleEmail: 'jdecker@inwesttitle.com',
+    listingAgent: 'Mark Bishop',
+    listingAgentPhone: 8016024201,
     listingAgentEmail: "mark@shine.com",
-    buyer:'Alex Doss', 
-    buyerPhone: 8012239999, 
+    buyer: 'Alex Doss',
+    buyerPhone: 8012239999,
     buyerEmail: 'alex_doss_42_@bob.com',
     checklist: [
       {
-       id: ++toDoId, 
-       text: 'Collect Earnest Money', 
-       isComplete: false, 
-        
+        id: ++toDoId,
+        text: 'Collect Earnest Money',
+        isComplete: false,
+
       },
       {
-       id: ++toDoId, 
-       text: 'Yell at agent', 
-       isComplete: false, 
-        
+        id: ++toDoId,
+        text: 'Yell at agent',
+        isComplete: false,
+
       },
       {
-       id: ++toDoId, 
-       text: 'Order Appraisal', 
-       isComplete: false, 
-        
+        id: ++toDoId,
+        text: 'Order Appraisal',
+        isComplete: false,
+
       },
     ]
-    
+
   },
   {
     id: ++id,
-    address: '648 S 500 W Provo UT', 
+    address: '648 S 500 W Provo UT',
     mls: 13288899,
-    price: 250000, 
-    lender:'Travis Carter', 
-    lenderPhone: 801339999, 
-    lenderEmail: 'travis@firstcolonymortgage.com', 
+    price: 250000,
+    lender: 'Travis Carter',
+    lenderPhone: 801339999,
+    lenderEmail: 'travis@firstcolonymortgage.com',
     title: "Jami Decker",
     titlePhone: 8012234400,
-    titleEmail: 'jdecker@inwesttitle.com', 
-    listingAgent: 'Mark Bishop', 
-    listingAgentPhone: 8016024201, 
+    titleEmail: 'jdecker@inwesttitle.com',
+    listingAgent: 'Mark Bishop',
+    listingAgentPhone: 8016024201,
     listingAgentEmail: "mark@shine.com",
-    buyer:'Andrew Westenskow', 
-    buyerPhone: 8012239999, 
+    buyer: 'Andrew Westenskow',
+    buyerPhone: 8012239999,
     buyerEmail: 'alex_doss_42_@bob.com',
     checklist: [
       {
-       id: ++toDoId, 
-       text: 'Collect Earnest Money', 
-       isComplete: false, 
-        
+        id: ++toDoId,
+        text: 'Collect Earnest Money',
+        isComplete: false,
+
       },
       {
-       id: ++toDoId, 
-       text: 'Yell at agent', 
-       isComplete: false, 
-        
+        id: ++toDoId,
+        text: 'Yell at agent',
+        isComplete: false,
+
       },
       {
-       id: ++toDoId, 
-       text: 'Order Appraisal', 
-       isComplete: false, 
-        
+        id: ++toDoId,
+        text: 'Order Appraisal',
+        isComplete: false,
+
       },
     ]
-    
+
   },
   {
     id: ++id,
-    address: '2112 Rush Ave, Toronto Canada', 
+    address: '2112 Rush Ave, Toronto Canada',
     mls: 13288899,
-    price: 250000, 
-    lender:'Travis Carter', 
-    lenderPhone: 801339999, 
-    lenderEmail: 'travis@firstcolonymortgage.com', 
+    price: 250000,
+    lender: 'Travis Carter',
+    lenderPhone: 801339999,
+    lenderEmail: 'travis@firstcolonymortgage.com',
     title: "Jami Decker",
     titlePhone: 8012234400,
-    titleEmail: 'jdecker@inwesttitle.com', 
-    listingAgent: 'Mark Bishop', 
-    listingAgentPhone: 8016024201, 
+    titleEmail: 'jdecker@inwesttitle.com',
+    listingAgent: 'Mark Bishop',
+    listingAgentPhone: 8016024201,
     listingAgentEmail: "mark@shine.com",
-    buyer:'Jacob Ankney', 
-    buyerPhone: 8012239999, 
+    buyer: 'Jacob Ankney',
+    buyerPhone: 8012239999,
     buyerEmail: 'alex_doss_42_@bob.com',
     checklist: [
       {
-       id: ++toDoId, 
-       text: 'Collect Earnest Money', 
-       isComplete: false, 
-        
+        id: ++toDoId,
+        text: 'Collect Earnest Money',
+        isComplete: false,
+
       },
       {
-       id: ++toDoId, 
-       text: 'Yell at agent', 
-       isComplete: false, 
-        
+        id: ++toDoId,
+        text: 'Yell at agent',
+        isComplete: false,
+
       },
       {
-       id: ++toDoId, 
-       text: 'Order Appraisal', 
-       isComplete: false, 
-        
+        id: ++toDoId,
+        text: 'Order Appraisal',
+        isComplete: false,
+
       },
     ]
-    
+
   },
-]
+]}
