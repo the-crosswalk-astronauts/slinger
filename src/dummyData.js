9264256a32bd1f6d5ae787f055e51c2291e78800
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-06-27 15:48:43 -0600

    added dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,122 @@
+let id=0
+let toDoId=0
+
+module.exports= [
+  {
+    id: ++id,
+    address: '512 N Claypool Ct VA Beach VA', 
+    mls: 13288899,
+    price: 250000, 
+    lender:'Travis Carter', 
+    lenderPhone: 801339999, 
+    lenderEmail: 'travis@firstcolonymortgage.com', 
+    title: "Jami Decker",
+    titlePhone: 8012234400,
+    titleEmail: 'jdecker@inwesttitle.com', 
+    listingAgent: 'Mark Bishop', 
+    listingAgentPhone: 8016024201, 
+    listingAgentEmail: "mark@shine.com",
+    buyer:'Alex Doss', 
+    buyerPhone: 8012239999, 
+    buyerEmail: 'alex_doss_42_@bob.com',
+    checklist: [
+      {
+       id: ++toDoId, 
+       text: 'Collect Earnest Money', 
+       isComplete: false, 
+        
+      },
+      {
+       id: ++toDoId, 
+       text: 'Yell at agent', 
+       isComplete: false, 
+        
+      },
+      {
+       id: ++toDoId, 
+       text: 'Order Appraisal', 
+       isComplete: false, 
+        
+      },
+    ]
+    
+  },
+  {
+    id: ++id,
+    address: '648 S 500 W Provo UT', 
+    mls: 13288899,
+    price: 250000, 
+    lender:'Travis Carter', 
+    lenderPhone: 801339999, 
+    lenderEmail: 'travis@firstcolonymortgage.com', 
+    title: "Jami Decker",
+    titlePhone: 8012234400,
+    titleEmail: 'jdecker@inwesttitle.com', 
+    listingAgent: 'Mark Bishop', 
+    listingAgentPhone: 8016024201, 
+    listingAgentEmail: "mark@shine.com",
+    buyer:'Andrew Westenskow', 
+    buyerPhone: 8012239999, 
+    buyerEmail: 'alex_doss_42_@bob.com',
+    checklist: [
+      {
+       id: ++toDoId, 
+       text: 'Collect Earnest Money', 
+       isComplete: false, 
+        
+      },
+      {
+       id: ++toDoId, 
+       text: 'Yell at agent', 
+       isComplete: false, 
+        
+      },
+      {
+       id: ++toDoId, 
+       text: 'Order Appraisal', 
+       isComplete: false, 
+        
+      },
+    ]
+    
+  },
+  {
+    id: ++id,
+    address: '2112 Rush Ave, Toronto Canada', 
+    mls: 13288899,
+    price: 250000, 
+    lender:'Travis Carter', 
+    lenderPhone: 801339999, 
+    lenderEmail: 'travis@firstcolonymortgage.com', 
+    title: "Jami Decker",
+    titlePhone: 8012234400,
+    titleEmail: 'jdecker@inwesttitle.com', 
+    listingAgent: 'Mark Bishop', 
+    listingAgentPhone: 8016024201, 
+    listingAgentEmail: "mark@shine.com",
+    buyer:'Jacob Ankney', 
+    buyerPhone: 8012239999, 
+    buyerEmail: 'alex_doss_42_@bob.com',
+    checklist: [
+      {
+       id: ++toDoId, 
+       text: 'Collect Earnest Money', 
+       isComplete: false, 
+        
+      },
+      {
+       id: ++toDoId, 
+       text: 'Yell at agent', 
+       isComplete: false, 
+        
+      },
+      {
+       id: ++toDoId, 
+       text: 'Order Appraisal', 
+       isComplete: false, 
+        
+      },
+    ]
+    
+  },
+]
