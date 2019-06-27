d771c6530a2ed4068e2cb09e3b46978e879f8ad8
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-06-27 16:31:31 -0600

    dummy data update

---
+++ src/Components/UserView/IndividualDeal/Checklist.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        Hey man this is the checklist 
+        
+
+
+      </div>
+    )
+  }
+}
---
+++ src/Components/UserView/IndividualDeal/ContactCard.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Contact Card</h1>
+        
+
+
+      </div>
+    )
+  }
+}
