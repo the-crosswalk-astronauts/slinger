4de58b844ec860156b459cf59e4df6785d32d109
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-06-27 15:49:28 -0600

    Sidebar basic

---
+++ src/Components/UserView/AndrewDummy/sidebarDummy.js
@@ -0,0 +1,60 @@
+module.exports = {
+  contracts: [
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    },
+    {
+      address: '123 fake street',
+      buyer: 'Bob bobby bob'
+    }
+  ]
+}
--- src/Components/UserView/Sidebar.jsx
+++ src/Components/UserView/Sidebar.jsx
@@ -1,14 +1,23 @@
 import React, {Component} from 'react'
+import {contracts} from './AndrewDummy/sidebarDummy'
 
 
 export default class Sidebar extends Component {
   render() {
     return (
-      <div>
-        <h1>Sidebar</h1>
-        
-
-
+      <div className='Sidebar'>
+        <div className="search-hold">
+          <input type="text" placeholder='Search your contracts'/>
+        </div>
+        <div className="sidebar-cards-hold">
+          {contracts.map(element => (
+            <div className='sidebar-card'>
+              <p>{element.buyer}</p>
+              <p>{element.address}</p>
+            </div>
+          ))}
+        </div>
+        <div className="sidebar-nav-hold"></div>
       </div>
     )
   }
