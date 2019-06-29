75bfe4c880621be0c93ed8807545078dba65b79b
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-06-29 12:02:42 -0600

    working on dealDisplay

--- src/Components/UserView/DealDisplay.jsx
+++ src/Components/UserView/DealDisplay.jsx
@@ -2,6 +2,12 @@ import React, {Component} from 'react'
 
 
 export default class DealDisplay extends Component {
+  constructor(){
+    super()
+    this.state={
+      activeDeal:''
+    }
+  }
   render() {
     return (
       <div>
