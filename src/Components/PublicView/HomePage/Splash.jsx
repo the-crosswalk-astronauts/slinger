9f6c62eda7585723bc7818d17370a5d2cd44b35b
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-06-28 22:24:04 -0600

    working on homepage

--- src/Components/PublicView/HomePage/Splash.jsx
+++ src/Components/PublicView/HomePage/Splash.jsx
@@ -5,7 +5,12 @@ export default class DealDisplay extends Component {
   render() {
     return (
       <div>
-        This is the home page BRUH
+        <NavBar/>
+
+        <QuickSell/>
+        
+        <Register/>
+
       </div>
     )
   }
