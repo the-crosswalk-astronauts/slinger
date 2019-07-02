1a346d1aedaff65090d4f01798281678514a6bef
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-07-02 15:38:29 -0600

    Created mongo models

--- server/models/ContractModel.js
+++ server/models/ContractModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const Contact = require('./ContactModel')
+const ToDo = require('./ToDoModel')
 
 const DealSchema = new mongoose.Schema({
   address: {type: 'string'},
