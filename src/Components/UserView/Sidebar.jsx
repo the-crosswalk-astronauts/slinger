728f42414c8091f801d5e5d4c628ce3faedaab41
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-07-02 18:48:30 -0600

    sidebar search now searches by address on the front end

--- src/Components/UserView/Sidebar.jsx
+++ src/Components/UserView/Sidebar.jsx
@@ -1,24 +1,34 @@
 import React, { Component } from 'react'
 import { deals } from '../../dummyData'
 import { updateDealDisplay } from '../../Redux/store'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 class Sidebar extends Component {
+  state = {
+    searchText: ''
+  }
 
-
+  changeHandler = (e) => {
+    this.setState({
+      searchText: e.target.value
+    })
+  }
 
   render() {
     return (
       <div className='Sidebar'>
         <div className="search-hold">
-          <input type="text" placeholder='Search your contracts' />
+          <input type="text" placeholder='Search your contracts' onChange={this.changeHandler} />
         </div>
         <div className="sidebar-cards-hold">
-          {deals.map(element => (
-            <div key={element.id} className='sidebar-card' onClick={()=>this.props.updateDealDisplay(element.id)}>
+          {deals.filter((el,i)=>{
+            return el.address.toLowerCase().includes(this.state.searchText.toLowerCase())
+          }).map(element => (    
+         <div key={element.id} className='sidebar-card' onClick={() => this.props.updateDealDisplay(element.id)}>
               <p>{element.buyer}</p>
               <p>{element.address}</p>
             </div>
+            
           ))}
         </div>
         <nav className="sidebar-nav-hold">
@@ -32,14 +42,14 @@ class Sidebar extends Component {
   }
 }
 
-const mapStateToProps=(state)=> {
+const mapStateToProps = (state) => {
   return {
 
   }
 }
 
-const mapDispatchToProps= {
-updateDealDisplay
+const mapDispatchToProps = {
+  updateDealDisplay
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Sidebar)
