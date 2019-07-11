eb6ae384d68499e7401db72ab2cdeed79648b7a0
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-2.local>
2019-07-11 11:06:45 -0600

    fixed universal search on vault component

--- src/Components/UserView/Vault/Vault.jsx
+++ src/Components/UserView/Vault/Vault.jsx
@@ -2,17 +2,40 @@ import React, { Component } from "react";
 import dummyData from "../../../dummyData";
 import DealCard from "../DealCard";
 import SearchBar from "./SearchBar";
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
+
+//after creating this component I think it would just be better to have a "show archived/vault" checkbox on the user home page. almost all of the logic is the same. 
 
 class Vault extends Component {
   render() {
-    let vaultDeals = dummyData.deals.filter(el=>{
-        return 
-    }).map(el => {
-      if (el.inVault) {
-        return <DealCard address={el.address} price={el.price} key={el.id} />;
-      }
-    });
+    let vaultDeals = dummyData.deals
+      .filter(el => {
+        let buyerIndex = el.contacts.findIndex(contact => {
+          return contact.type.toLowerCase() === "buyer";
+        });
+
+        let listingAgentIndex = el.contacts.findIndex(contact => {
+          return contact.type.toLowerCase() === "listing agent";
+        });
+
+        let { searchVal } = this.props;
+        searchVal.toLowerCase();
+        if (
+          el.contacts[buyerIndex].name.toLowerCase().includes(searchVal) ||
+          el.contacts[listingAgentIndex].name
+            .toLowerCase()
+            .includes(searchVal) ||
+          el.address.toLowerCase().includes(searchVal) ||
+          el.mls.toString().includes(searchVal)
+        ) {
+          return el;
+        }
+      })
+      .map(el => {
+        if (el.inVault) {
+          return <DealCard address={el.address} price={el.price} key={el.id} />;
+        }
+      });
 
     return (
       <div>
@@ -24,14 +47,15 @@ class Vault extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        searchVal:state.vaultSearch
-    }
-  }
-  
-  const mapDispatchToProps = {
-    
-  }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(Vault)
+const mapStateToProps = state => {
+  return {
+    searchVal: state.vaultSearch
+  };
+};
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Vault);
